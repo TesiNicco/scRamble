f3cb1d687599ada1fcf0707fdecba82512828357
TesiNicco <holstegelab-ntesi@ui-02.spider.surfsara.nl>
2025-03-04 16:16:12 +0100

    fix issue with fid

--- bin/scRamble_sexchr.R
+++ bin/scRamble_sexchr.R
@@ -7,11 +7,10 @@ args = commandArgs(trailingOnly = TRUE)
 ### Functions
 # Function to take the VCF file (inputation input) and generate 22 PLINK files (1 per chromosome)
 function_split <- function(i, finp, out){
-        if (i == 1){ cmd = paste0("plink2 --vcf ", finp, " --real-ref-alleles --make-bed --out ", out, "/chrAll_input --threads 4"); system(cmd, ignore.stdout=T) }
+        if (i == 1){ cmd = paste0("plink --vcf ", finp, " --real-ref-alleles --make-bed --out ", out, "/chrAll_input --threads 4"); system(cmd, ignore.stdout=T) }
         if (i == 23){ i = "X" }
         cmd = paste0("plink2 --bfile ", out, "/chrAll_input --chr ", i, " --real-ref-alleles --make-pgen --out ", out, "/chr", i, "_input --threads 4")
         system(cmd, ignore.stdout=T)
-
         print(paste0("##        Done with chromosome ", i))
         return("Done with splitting")
 }
@@ -23,7 +22,10 @@ function_generate <- function(out, sex_chromosomes){
         listFiles <- system(paste0("ls ", out, "/*psam"), intern=T)
 
         # open the first
-        d <- fread(listFiles[1], h=T)
+        d <- fread(listFiles[1], h=T, check.names=F)
+
+        # add new family ID as 0
+        d$FAID = 0
 
         # generate as many random numbers as the number of samples
         set.seed(1234)
@@ -45,8 +47,23 @@ function_generate <- function(out, sex_chromosomes){
 
 # Function to scramble the individual IDs of ea chromosome
 function_scramble <- function(i, key, out){
+        # make sure key is a dataframe
+        key = data.frame(key, check.names=F)
+
+        # find the index of the chromosome
+        index_chrom = which(colnames(key) == paste0('CHR', i))
+
+        # find the index of the old FID column
+        index_fid = grep('FID', colnames(key))
+
+        # find the index of the IID column
+        index_id = which(colnames(key) == 'IID')
+
+        # find the index of the new FID
+        index_newfid = which(colnames(key) == 'FAID')
+
         # generate temporary file for the new list of keys
-        write.table(key[, c(1, ..i+2)], paste0(out, "/tmp_key.txt"), quote=F, row.names=F, sep="\t")
+        write.table(key[, c(index_fid, index_id, index_newfid, index_chrom)], paste0(out, "/tmp_key.txt"), quote=F, row.names=F, sep="\t")
 
         if (i == 23){ i = "X" }
 
@@ -109,25 +126,26 @@ if (args[3] %in% c("yes", "YES")){
 print("## Done with splitting main VCF into chromosome-specific PLINK files.")
 
 # Second is the generation of the random numbers and their distribution across chromosomes
-print("## Start with generation of random numbers")
+print("## Start with generation of random numbers.")
 key <- function_generate(args[2], args[3])
-print("## Done with generation of random numbers")
+print("## Done with generation of random numbers.")
 
 # Third we need to loop across chromosome and do the scrambling
-print("## Start with scrambling IDs of each PLINK file")
+print("## Start with scrambling IDs of each PLINK file.")
 if (args[3] %in% c("yes", "YES")){ 
     res <- lapply(1:23, function_scramble, key = key, out = args[2])
 } else {
     res <- lapply(1:22, function_scramble, key = key, out = args[2])
 }
-print("## Done with scrambling IDs of each PLINK file")
+print("## Done with scrambling IDs of each PLINK file.")
 
 # Fourth we need to re-merge all files together
 print("## Start with merging.")
 res <- function_merge(args[2], args[3])
-print("## Done with merging. A VCF file with scrambled individual IDs has been created")
+print("## Done with merging. A VCF file with scrambled individual IDs has been created.")
 
-# Clean shit up
+# Clean things up
+print("## Removing redundant file.")
 try(system(paste0("rm ", args[2], "/fileList_toMerge.txt")), silent=T)
 try(system(paste0("rm ", args[2], "/*nosex")), silent=T)
 try(system(paste0("for chr in {1..22}; do rm ", args[2], "/chr${chr}_*; done")), silent=T)
@@ -139,6 +157,7 @@ try(system(paste0("rm ", args[2], "/*fam")), silent=T)
 
 # Finally add 'chr' if Reference genome is GRCh38 and update the contig IDs
 if (args[4] %in% c("GRCh38", "hg38", "HG38")){
+    print("## Adding chr in front of each chromosome as data is GRCh38.")
     cmd = paste0("zcat ", args[2], "/chrAll_input_scrambled.vcf.gz | awk '{if($0 !~ /^#/) print \"chr\"$0; else print $0}' > ", args[2], "/chrAll_input_scrambled.vcf")
     system(cmd)
     cmd = paste0("sed 's/contig=<ID=/contig=<ID=chr/g' ", args[2], "/chrAll_input_scrambled.vcf | sed 's/ID=chr23/ID=chrX/g' > ", args[2], "/chrAll_input_scrambled_updated.vcf")
@@ -149,6 +168,7 @@ if (args[4] %in% c("GRCh38", "hg38", "HG38")){
 
 # Last thing is that if chrX is there it is called chr23 --> change the name now
 if (args[3] %in% c("yes", "YES")){
+    print("## Fixing chromosome X as this is included in the data.")
     cmd = paste0("sed 's/chr23/chrX/g' ", args[2], "/chrAll_input_scrambled.vcf > ", args[2], "/chrAll_input_scrambled_updated.vcf")
     system(cmd)
     system(paste0("rm ", args[2], "/chrAll_input_scrambled.*"))
@@ -156,10 +176,14 @@ if (args[3] %in% c("yes", "YES")){
 }
 
 # Finally, sort, index and divide in chromosomes
+print("## Sorting data.")
 cmd_sort = paste0("bcftools sort -Oz -o ", args[2], "/chrAll_input_scrambled_sorted.vcf.gz ", args[2], "/chrAll_input_scrambled.vcf")
 system(cmd_sort)
+print("## Indexing data.")
 cmd_index = paste0("bcftools index ", args[2], "/chrAll_input_scrambled_sorted.vcf.gz ")
 system(cmd_index)
+print("## Splitting chromosomes.")
 cmd_divide = paste0("for chr in {1..22} X; do bcftools view ", args[2], "/chrAll_input_scrambled_sorted.vcf.gz --regions chr${chr} -Oz -o ", args[2], "/chr${chr}_input_scrambled.vcf.gz; done")
 system(cmd_divide)
+print("## Cleaning data.")
 system(paste0("rm ", args[2], "/chrAll*"))
